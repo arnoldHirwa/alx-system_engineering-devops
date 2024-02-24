81af0fd477e129ae4345f9c5e5169153e82e3d04
Hirwa Arnold <arnold.hirw@gmail.com>
2024-02-24 04:42:55 +0200

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/pup
 # A file to install flask using Puppet
 
-package { 'flask',
-    ensure => '2.1.0',
-    provider => 'pip3',
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
