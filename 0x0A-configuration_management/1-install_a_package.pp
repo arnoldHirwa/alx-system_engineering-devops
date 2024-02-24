0483cf6f0d1c376b56aba712a28fe43affe2595f
Hirwa Arnold <arnold.hirw@gmail.com>
2024-02-24 04:49:30 +0200

    try agin

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-#!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+exec { 'install flask':
+  command => 'pip3 install flask==2.1.0',
+  path    => '/usr/bin/',
+  unless  => 'pip3 list | grep "Flask (2.1.0)"',
 }
