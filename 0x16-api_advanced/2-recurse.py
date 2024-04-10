5e0e42944ec2fd4dd720c37e7011ad33532bd6f6
Hirwa Arnold <arnold.hirw@gmail.com>
2024-04-10 02:40:47 +0200

    recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,10 +4,11 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """Return titles of all hot posts on a given subreddit."""
+    """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
     }
     params = {
         "after": after,
