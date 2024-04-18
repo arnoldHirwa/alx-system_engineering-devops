2dbdc0de7b9df3aa3b05fe2544d3360ca24d4c71
Hirwa Arnold <arnold.hirw@gmail.com>
2024-04-19 00:02:46 +0200

    Solve problem of high request and amount of files opened

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# handle problem of big amount of requests
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
